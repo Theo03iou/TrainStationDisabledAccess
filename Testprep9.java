197cb9aef7864ee4bb1c35b833aba8a7e08f98e5
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2023-01-04 19:28:02 +0000

    Update Testprep9.java

--- Testprep9.java
+++ Testprep9.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 class station {
-    String name = "";
-    boolean access = false;
+    String name;
+    boolean accessibility;
 }
 
-public class Testprep9 {
+public class stationProgram {
     public static void main(String[] args) {
         start();
     }
@@ -14,53 +14,73 @@ public class Testprep9 {
 
         System.out.println("How many stations do you want to enter?");
         Scanner scanner = new Scanner(System.in);
-        int stationLeng = scanner.nextInt();
-        station [] newStation = new station[stationLeng];
+        int numStations = scanner.nextInt();
+        station[] newStation = new station[numStations];
 
-        for (int i = 0; i < stationLeng; i++) {
-            newStation[i] = new station();
+        for (int i = 0; i <= numStations; i++) {
+            int j = i + 1;
+            if (j <= numStations) {
+                newStation[i] = new station();
 
-            System.out.println("Enter the name of an underground station:");
-            String stationName2 = scanner.nextLine();
-            String stationName = scanner.nextLine();
+                System.out.println("Station " + j + "/" + numStations);
+                System.out.println("Enter the name of the station");
+                String dwad = scanner.nextLine();
+                String stationName = scanner.nextLine();
 
-            setName(stationName, i, newStation);
+                if (stationName.equals("stop")) {
+                    nextStage(newStation);
+                    System.exit(0);
+                }
+                
+                System.out.println("Does the station have step-free access?");
+                boolean stepFree = scanner.nextBoolean();
 
-            System.out.println("Does " + stationName + " have step-free access?\nJust enter true or false");
-            boolean stepFree = scanner.nextBoolean();
-
-            setAccess(stepFree, i, newStation);
-            
+                setStation(stationName, stepFree, i, newStation);
+            }
         }
         nextStage(newStation);
     }
-    
-    
+
     private static void nextStage(station[] newStation) {
-        for (int i = 0; i < newStation.length; i++) {
-            String name = getName(newStation, i);
-            boolean access = getAccess(newStation, i);
-            System.out.println(name + " Station has step-free access: " + access);
+        System.out.println("Choose from the following:\n[1]: Exit the program\n[2]: Find a specific station's step-free access\n[3] Print all with step-free access");
+        Scanner scanner = new Scanner(System.in);
+        int userChoice = scanner.nextInt();
 
+        if (userChoice == 2) {
+            System.out.println("What station access would you like to find out?");
+            for (int i = 0; i < newStation.length; i++) {
+                String currentStation = "[" + (i + 1) + "]: " + getStationName(newStation, i);
+                System.out.println(currentStation);
+            }
+            int stationChoice = scanner.nextInt();
+
+                if (stationChoice > 0 && stationChoice <= newStation.length) {
+                    System.out.println(getStationName(newStation, userChoice - 1) + " station has disabled access: " + getStationAccess(newStation, userChoice - 1));
+                }
+        } else if (userChoice == 3) {
+            for (int i = 0; i < newStation.length; i++) {
+                if (newStation[i].accessibility == true) {
+                    System.out.println(getStationName(newStation, i) + " station has disabled access: " + getStationAccess(newStation, i));
+                }
+            }
         }
-        System.exit(0);
     }
-    
-    
-    private static void setAccess(boolean stepFree, int i, station[] newStation) {
-        newStation[i].access = stepFree;
-     }
 
-    private static void setName(String stationName, int i, station newStation[]) {
+    private static String getStationName(station[] newStation, int i) {
+        String stationName = newStation[i].name;
+        return stationName;
+    }
+
+    private static boolean getStationAccess(station[] newStation, int i) {
+        boolean stationAccess = newStation[i].accessibility;
+        return stationAccess;
+    }
+
+
+    private static void setStation(String stationName, boolean stepFree, int i, station[] newStation) {
         newStation[i].name = stationName;
-    }
-    
-    private static boolean getAccess(station[] newStation, int i) {
-        return newStation[i].access;
+        newStation[i].accessibility = stepFree;
     }
 
-    private static String getName(station[] newStation, int i) {
-        return newStation[i].name;
-    }
- 
 }
+
